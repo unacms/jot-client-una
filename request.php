9489634438cc65bb98ed7d657503294b3b06818d
AntonLV <vlnotna@gmail.com>
2017-09-13 13:22:55 +0300

    Update script.

--- request.php
+++ request.php
@@ -3,8 +3,8 @@
  * Copyright (c) UNA, Inc - https://una.io
  * MIT License - https://opensource.org/licenses/MIT
  *
- * @defgroup    Timeline Timeline
- * @ingroup     UnaModules
+ * @defgroup	Messenger Messenger
+ * @ingroup		UnaModules
  *
  * @{
  */

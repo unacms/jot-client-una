2d5b1fa0f8c88f86ae376c23fc265a9877c7d724
AntonLV <vlnotna@gmail.com>
2017-12-20 11:08:45 +0300

    Modules updates.

--- install/config.php
+++ install/config.php
@@ -17,7 +17,7 @@ $aConfig = array(
 	'name' => 'bx_messenger',
 	'title' => 'Messenger',
 	'note' => 'Messenger module.',
-	'version' => '9.0.2.DEV',
+	'version' => '9.0.3.DEV',
 	'vendor' => 'BoonEx',
 	'help_url' => 'http://feed.una.io/?section={module_name}',
 

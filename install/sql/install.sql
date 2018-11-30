7fc1dccb1d5d59281f364d85bd462ccbbcfad79d
AlexT <at@una.io>
2018-11-30 15:07:22 +1100

    Allow negative values in comments to allow anonymous posting

--- install/sql/install.sql
+++ install/sql/install.sql
@@ -115,7 +115,7 @@ CREATE TABLE IF NOT EXISTS `bx_messenger_lcomments` (
   `lcmt_system_id` int(11) NOT NULL DEFAULT '0',
   `lcmt_vparent_id` int(11) NOT NULL DEFAULT '0',
   `lcmt_object_id` int(11) NOT NULL DEFAULT '0',
-  `lcmt_author_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `lcmt_author_id` int(11) NOT NULL DEFAULT '0',
   `lcmt_level` int(11) NOT NULL DEFAULT '0',
   `lcmt_text` text NOT NULL,
   `lcmt_time` int(11) unsigned NOT NULL DEFAULT '0',

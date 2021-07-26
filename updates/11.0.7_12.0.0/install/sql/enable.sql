6ffca7201c33f292f703cda349f0869e4c26c596
AlexeyUna <ad@una.io>
2021-07-26 17:42:32 +0300

    Update script from 11.0.7 to 12.0.0

--- updates/11.0.7_12.0.0/install/sql/enable.sql
+++ updates/11.0.7_12.0.0/install/sql/enable.sql
@@ -55,8 +55,7 @@ UPDATE `sys_pages_blocks` SET `designbox_id`=11 WHERE `object` != 'bx_messenger_
 
 DELETE `sys_acl_actions`, `sys_acl_matrix`
 FROM `sys_acl_actions`, `sys_acl_matrix`
-WHERE `sys_acl_matrix`.`IDAction` = `sys_acl_actions`.`ID` AND `sys_acl_actions`.`Module` = @sName;
-DELETE FROM `sys_acl_actions` WHERE `Module` = @sName AND `Name` IN ('video recorder', 'join vc', 'join personal vc');
+WHERE `sys_acl_matrix`.`IDAction` = `sys_acl_actions`.`ID` AND `sys_acl_actions`.`Module` = @sName AND `Name` IN ('video recorder', 'join vc', 'join personal vc');
 
 -- ACL
 

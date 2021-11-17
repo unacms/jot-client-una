71e38c4bca89c7541e0795149977032635c827a3
AlexeyUna <ad@una.io>
2021-11-17 12:42:43 +0300

    Update script from 12.0.2 to 12.0.3

---
+++ updates/12.0.2_12.0.3/install/config.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Copyright (c) UNA, Inc - https://una.io
+ * MIT License - https://opensource.org/licenses/MIT
+ */
+
+$aConfig = array(
+    /**
+     * Main Section.
+     */
+    'title' => 'Messenger',
+    'version_from' => '12.0.2',
+	'version_to' => '12.0.3',
+    'vendor' => 'BoonEx',
+
+    'compatible_with' => array(
+        '12.0.0-B1'
+    ),
+
+    /**
+     * 'home_dir' and 'home_uri' - should be unique. Don't use spaces in 'home_uri' and the other special chars.
+     */
+    'home_dir' => 'boonex/messenger/updates/update_12.0.2_12.0.3/',
+	'home_uri' => 'messenger_update_1202_1203',
+
+	'module_dir' => 'boonex/messenger/',
+	'module_uri' => 'messenger',
+
+    'db_prefix' => 'bx_messenger_',
+    'class_prefix' => 'BxMessenger',
+	
+	'menu_triggers' => array(
+        'trigger_profile_view_actions',
+    ),
+	
+  /**
+     * Installation/Uninstallation Section.
+     */
+    'install' => array(
+		'execute_sql' => 1,
+        'update_files' => 1,
+        'update_languages' => 1,
+		'clear_db_cache' => 1
+    ),
+	
+	/**
+     * Category for language keys.
+     */
+    'language_category' => 'Messenger',
+	
+	'delete_files' => array(),
+);
---
+++ updates/12.0.2_12.0.3/install/sql/enable.sql
@@ -0,0 +1,28 @@
+SET @sName = 'bx_messenger';
+
+DELETE FROM `sys_options` WHERE `name` = 'bx_messenger_show_friends';
+INSERT INTO `sys_options` (`name`, `value`, `category_id`, `caption`, `type`, `check`, `check_error`, `extra`, `order`) VALUES
+('bx_messenger_show_friends', 'on', @iCategId, '_bx_messenger_show_friends', 'checkbox', '', '', '', 20);
+
+UPDATE `sys_options` SET `order` = 21 WHERE `name` = 'bx_messenger_remove_messages_immediately' AND `category_id` = @iCategId;
+UPDATE `sys_options` SET `order` = 22 WHERE `name` = 'bx_messenger_use_embedly' AND `category_id` = @iCategId;
+UPDATE `sys_options` SET `order` = 23 WHERE `name` = 'bx_messenger_giphy_key' AND `category_id` = @iCategId;
+UPDATE `sys_options` SET `order` = 24 WHERE `name` = 'bx_messenger_giphy_type' AND `category_id` = @iCategId;
+UPDATE `sys_options` SET `order` = 25 WHERE `name` = 'bx_messenger_giphy_content_rating' AND `category_id` = @iCategId;
+UPDATE `sys_options` SET `order` = 26 WHERE `name` = 'bx_messenger_giphy_limit' AND `category_id` = @iCategId;
+UPDATE `sys_options` SET `order` = 27 WHERE `name` = 'bx_messenger_emoji_set' AND `category_id` = @iCategId;
+UPDATE `sys_options` SET `order` = 28 WHERE `name` = 'bx_messenger_reactions_size' AND `category_id` = @iCategId;
+UPDATE `sys_options` SET `order` = 29 WHERE `name` = 'bx_messenger_show_emoji_preview' AND `category_id` = @iCategId;
+UPDATE `sys_options` SET `order` = 30 WHERE `name` = 'bx_messenger_jitsi_enable' AND `category_id` = @iCategId;
+UPDATE `sys_options` SET `order` = 31 WHERE `name` = 'bx_messenger_jitsi_server' AND `category_id` = @iCategId;
+UPDATE `sys_options` SET `order` = 32 WHERE `name` = 'bx_messenger_jitsi_chat' AND `category_id` = @iCategId;
+UPDATE `sys_options` SET `order` = 33 WHERE `name` = 'bx_messenger_jitsi_sync' AND `category_id` = @iCategId;
+UPDATE `sys_options` SET `order` = 34 WHERE `name` = 'bx_messenger_jitsi_hide_info' AND `category_id` = @iCategId;
+UPDATE `sys_options` SET `order` = 35 WHERE `name` = 'bx_messenger_jitsi_enable_watermark' AND `category_id` = @iCategId;
+UPDATE `sys_options` SET `order` = 36 WHERE `name` = 'bx_messenger_jitsi_watermark_link' AND `category_id` = @iCategId;
+UPDATE `sys_options` SET `order` = 37 WHERE `name` = 'bx_messenger_jitsi_only_for_private' AND `category_id` = @iCategId;
+UPDATE `sys_options` SET `order` = 38 WHERE `name` = 'bx_messenger_jitsi_support_url' AND `category_id` = @iCategId;
+UPDATE `sys_options` SET `order` = 39 WHERE `name` = 'bx_messenger_disable_contact_privacy' AND `category_id` = @iCategId;
+UPDATE `sys_options` SET `order` = 40 WHERE `name` = 'bx_messenger_enable_mentions' AND `category_id` = @iCategId;
+UPDATE `sys_options` SET `order` = 41 WHERE `name` = 'bx_messenger_jwt_app_id' AND `category_id` = @iCategId;
+UPDATE `sys_options` SET `order` = 42 WHERE `name` = 'bx_messenger_jwt_app_secret' AND `category_id` = @iCategId;

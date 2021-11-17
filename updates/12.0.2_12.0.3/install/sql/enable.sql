7e3d2df8cda1a4f47437ce7b45ce500606482179
AlexeyUna <ad@una.io>
2021-11-17 13:29:36 +0300

    Fixes for update script

--- updates/12.0.2_12.0.3/install/sql/enable.sql
+++ updates/12.0.2_12.0.3/install/sql/enable.sql
@@ -1,6 +1,8 @@
 SET @sName = 'bx_messenger';
 
 DELETE FROM `sys_options` WHERE `name` = 'bx_messenger_show_friends';
+
+SET @iCategId = (SELECT `id` FROM `sys_options_categories` WHERE `name`=@sName LIMIT 1);
 INSERT INTO `sys_options` (`name`, `value`, `category_id`, `caption`, `type`, `check`, `check_error`, `extra`, `order`) VALUES
 ('bx_messenger_show_friends', 'on', @iCategId, '_bx_messenger_show_friends', 'checkbox', '', '', '', 20);
 

3b2474685096b954cd5b18f172fea305af0be924
AlexeyUna <ad@una.io>
2024-02-20 13:58:31 +0300

    Ticket #223

--- template/css/tailwind/tailwind.config.js
+++ template/css/tailwind/tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: [require('./parent.config.js')],
-  safelist: ['md:col-span-3', 'xl:col-span-2', 'xl:col-span-3', 'xl:col-span-4', 'xl:col-span-5', 'xl:col-span-6', 'xl:col-span-7', 'xl:col-span-8'],
+  safelist: ['xl:block','md:col-span-3', 'xl:col-span-2', 'xl:col-span-3', 'xl:col-span-4', 'xl:col-span-5', 'xl:col-span-6', 'xl:col-span-7', 'xl:col-span-8'],
   theme: {
       extend: {
           colors: {
@@ -14,8 +14,5 @@ module.exports = {
               'xs': '480px'
           }
       },
-  },
-  /*blocklist: [ 'hidden', 'text-white', 'font-semibold', 'align-middle', 'align-top', 'flex-col', 'flex-nowrap', 'items-start', 'items-center',
-               'justify-center', 'justify-between', 'resize', 'visible', 'block', 'inline-block', 'flex', 'flex-grow', 'self-center', 'truncate',
-               'h-min', 'text-ellipsis', 'rounded-full', 'overflow-hidden' ]*/
+  }
 }

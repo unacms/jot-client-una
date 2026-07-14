9b0c69110fe52c404e764c1f62b872bc8406b286
Aleksandr Trofimov <at@unacms.com>
2026-07-14 13:50:36 +0600

    NEO app config

---
+++ install/sql/enable-app.sql
@@ -0,0 +1,17 @@
+
+-- PAGES: config_api
+
+UPDATE `sys_objects_page` SET `config_api`='{"layout":"messenger"}' WHERE `object`='bx_messenger_main';
+
+-- PAGES: active_api
+
+UPDATE `sys_pages_blocks` SET `active_api`=1 WHERE `object`='bx_messenger_main' AND `module`='bx_messenger' AND `title`='_bx_messenger_page_main_messenger_block';
+
+-- MENUS: active_api
+
+UPDATE `sys_menu_items` SET `active_api`=1 WHERE `set_name`='bx_messenger_nav_menu' AND `module`='bx_messenger' AND `name`='saved';
+UPDATE `sys_menu_items` SET `active_api`=1 WHERE `set_name`='bx_messenger_nav_menu' AND `module`='bx_messenger' AND `name`='reply';
+UPDATE `sys_menu_items` SET `active_api`=1 WHERE `set_name`='bx_messenger_nav_menu' AND `module`='bx_messenger' AND `name`='threads';
+UPDATE `sys_menu_items` SET `active_api`=1 WHERE `set_name`='bx_messenger_nav_menu' AND `module`='bx_messenger' AND `name`='inbox';
+UPDATE `sys_menu_items` SET `active_api`=1 WHERE `set_name`='bx_messenger_nav_menu' AND `module`='bx_messenger' AND `name`='direct';
+
